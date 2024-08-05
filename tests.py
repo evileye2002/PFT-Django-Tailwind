0f022890ee693b575f01549349828f0b25d785fb
3vil3y3 <101619460+evileye2002@users.noreply.github.com>
2024-08-05 23:06:42 +0700

    v0.1.3.1 test sign in

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
 
 # class HostTest(LiveServerTestCase):
@@ -14,22 +15,49 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class SignInTest(LiveServerTestCase):
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(self):
         super().setUpClass()
-        cls.selenium = webdriver.Chrome()
-        cls.selenium.implicitly_wait(10)
+        self.timeout = 10
+        self.options = Options()
+        # self.options.add_argument("--headless")
 
-    def test_form(self):
-        self.selenium.get("http://localhost:8000/account/sign-in")
+        self.selenium = webdriver.Chrome(self.options)
+        self.selenium.implicitly_wait(self.timeout)
 
-        username_input = self.selenium.find_element(By.NAME, "username")
-        password_input = self.selenium.find_element(By.NAME, "password")
-        sign_in_btn = self.selenium.find_element(By.ID, "btn-sign-in")
+    def test_case_01(self):
+        driver = self.selenium
+        driver.get("http://localhost:8000/account/sign-in")
+
+        username_input = driver.find_element(By.NAME, "username")
+        password_input = driver.find_element(By.NAME, "password")
+        sign_in_btn = driver.find_element(By.ID, "btn-sign-in")
 
         username_input.send_keys("admin")
         password_input.send_keys("kh@nh2002")
         sign_in_btn.click()
 
-        WebDriverWait(self.selenium, 15).until(
+        WebDriverWait(driver, self.timeout).until(
             lambda driver: "Số dư" in driver.page_source
+        )
+
+    def test_case_02(self):
+        driver = self.selenium
+
+        driver.get("http://localhost:8000/account/sign-out")
+        WebDriverWait(driver, self.timeout).until(
+            lambda driver: "Đăng nhập" in driver.page_source
+        )
+
+        driver.get("http://localhost:8000/account/sign-in")
+
+        username_input = driver.find_element(By.NAME, "username")
+        password_input = driver.find_element(By.NAME, "password")
+        sign_in_btn = driver.find_element(By.ID, "btn-sign-in")
+
+        username_input.send_keys("admin")
+        password_input.send_keys("kh@nh2001")
+        sign_in_btn.click()
+
+        WebDriverWait(driver, self.timeout).until(
+            lambda driver: "thất bại" in driver.page_source
         )
